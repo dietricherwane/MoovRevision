d87c5e29a4ba199e0c20adaee63cb41fc0354da8
dietrich <erwan@ubuntu.(none)>
2014-10-30 17:34:21 +0000

    changes

--- lib/modules/error.rb
+++ lib/modules/error.rb
@@ -1,52 +1,52 @@
 module Error
-  
+
   def self.valid_gaming_session(gaming_session, screen_id)
     return xml_skeleton("MOOV REVISION-\n\nVous avez une session de jeu active jusqu'au: #{gaming_session.expires_at.strftime("%d-%m-%y") rescue nil}. Pour annuler cette inscription, envoyez: STOP.", screen_id)
   end
-  
+
   def self.create_account(screen_id)
     return xml_skeleton("MOOV REVISION-4\n\nVeuillez réessayer plus tard.", screen_id)
   end
-  
+
   def self.create_gaming_session(screen_id)
     return xml_skeleton("MOOV REVISION-5\n\nVeuillez réessayer plus tard.", screen_id)
   end
-  
+
   def self.billing(screen_id)
     return xml_skeleton("MOOV REVISION-3\n\nVotre compte n'a pas pu être débité.", screen_id)
   end
-  
+
   def self.xml_skeleton(text, screen_id)
     text = URI.escape(text)
-    
+
     return "<?xml version='1.0' encoding='utf-8'?>
     <response>
       <screen_type>form</screen_type>
-      <text>'#{text}'</text>
+      <text>#{text}</text>
       <session_op>end</session_op>
       <screen_id>#{screen_id}</screen_id>
     </response>
     "
   end
-  
+
   def self.session_over
     return "MOOV REVISION\nVous avez répondu à 20 questions. Veuillez attendre demain pour pouvoir rejouer."
   end
-  
+
   def self.create_an_account_first
     return "Veuillez créer un compte au préalable en envoyant XXX au XXXXX."
   end
-  
+
   def self.could_not_be_billed
     return "Votre compte n'a pas pu être débité. Veuillez recharger et réessayer."
   end
-  
+
   def self.gaming_notice
     return "Pour répondre aux questions, envoyez a ou b au xxxxx"
   end
-  
+
   def self.disable_account
-    return "MOOV REVISION\nVotre compte a été désactivé. N'hésitez pas à souscrire de nouveau." 
+    return "MOOV REVISION\nVotre compte a été désactivé. N'hésitez pas à souscrire de nouveau."
   end
 
 end
--- lib/modules/registration.rb
+++ lib/modules/registration.rb
@@ -1,19 +1,19 @@
 module Registration
-  
+
   def self.validate_registration(screen_id)
     return xml_skeleton("form", "Bienvenue au jeu MOOV REVISION. Votre inscription a bien été prise en compte. Répondez aux questions, cumulez le maximum de points et gagnez de nombreux lots.", [], 0, "end", screen_id)
   end
-  
+
   def self.xml_skeleton(screen_type, text, menu_options, back_link, session_op, screen_id)
     text = URI.escape(text)
     unless menu_options.blank?
       my_menu = get_menu(menu_options)
     end
-    
+
     return "<?xml version='1.0' encoding='utf-8'?>
     <response>
       <screen_type>#{screen_type}</screen_type>
-      <text>'#{text}'</text>
+      <text>#{text}</text>
       #{my_menu}
       <back_link>#{back_link}</back_link>
       <home_link>0</home_link>
@@ -22,7 +22,7 @@ module Registration
     </response>
     "
   end
-  
+
   def self.get_menu(menu_options)
     my_menu = "<options>"
     menu_options.each do |menu_option|
